521c66d7e7a56af86db32f353aa8be01607a2cb9
zul-m <zul-m@icloud.com>
2023-10-30 20:02:29 +0800

    Fix HeroSection

--- components/SlideUp.tsx
+++ components/SlideUp.tsx
@@ -1,36 +1,43 @@
-"use client" // this is a client component
+"use client"; // this is a client component
+import React, { useEffect, useRef, ReactNode } from "react";
 
-import React, { useEffect, useRef, ReactNode } from "react"
 interface Props {
-  offset?: string
-  children?: ReactNode
+  offset?: string;
+  children?: ReactNode;
   // any props that come into the component
 }
 
 export default function SlideUp({ children, offset = "0px" }: Props) {
-  const ref = useRef(null)
+  const ref = useRef(null);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
-            entry.target.classList.remove("opacity-0")
-            entry.target.classList.add("animate-slideUpCubiBezier")
+            entry.target.classList.remove("opacity-0");
+            entry.target.classList.add("animate-slideUpCubiBezier");
           }
-        })
+        });
       },
       { rootMargin: offset }
-    )
+    );
 
     if (ref.current) {
-      observer.observe(ref.current)
+      observer.observe(ref.current);
     }
-  }, [ref])
+
+    // Clean up the observer when the component unmounts
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, [ref, offset]); // Include 'ref' and 'offset' in the dependency array
 
   return (
     <div ref={ref} className="relative opacity-0">
       {children}
     </div>
-  )
+  );
 }
