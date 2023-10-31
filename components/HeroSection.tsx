dd0e5d7767d7686902292335bdda612b375e7c8e
zul-m <zul-m@icloud.com>
2023-10-31 12:32:01 +0800

    Change semicolon

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -28,15 +28,15 @@ const HeroSection = () => {
             </div>
         </div>
         <div className="flex flex-row items-center text-center justify-center ">
-        {<Link
+        <Link
         to="about"
         activeClass="active"
         spy={true}
         smooth={true}
         offset={-100}
         duration={500}>
-        <HiArrowDown size={35} className="animate-bounce" />
-        </Link>}
+        {<HiArrowDown size={35} className="animate-bounce" />}
+        </Link>
       </div>
     </section>
   )
