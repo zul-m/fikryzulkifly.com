437e51b8e5e1b979935df520648a03e26a782296
zul-m <zul-m@icloud.com>
2023-10-31 15:50:45 +0800

    npm install react-icons --save

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React from "react"
 import Image from "next/image"
-import { Link, LinkProps } from "react-scroll"
+import { Link } from "react-scroll/modules"
 import { HiArrowDown } from "react-icons/hi"
 
 const HeroSection = () => {
