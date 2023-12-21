faf8232f43a486d2f12f3aefbc36a9d66a38c972
Zulfikar Muhamad <zul-m@icloud.com>
2023-12-21 10:06:41 +0800

    Betulkan padding dan text size

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { FEATURES } from "@/styles/constants";
-import SlideUp from "./SlideUp";
 
 const HeroSection = () => {
   return (
@@ -9,7 +8,7 @@ const HeroSection = () => {
       className="w-full text-center items-center justify-center"
     >
       <div className="bg-gradient-to-b from-cyan-900 from-90% w-full px-5 pt-32 md:pt-40 pb-80">
-        <h1 className="max-w-7xl mx-auto text-start font-poppins font-bold text-4xl md:text-7xl">
+        <h1 className="max-w-7xl mx-auto text-start font-poppins font-bold text-3xl sm:text-5xl md:text-7xl">
           <span className="text-neutral-300 leading-tight">
             Kami menyediakan khidmat konsultasi berkaitan{" "}
           </span>
@@ -18,18 +17,16 @@ const HeroSection = () => {
           </span>
         </h1>
       </div>
-      <SlideUp offset="-300px 0px -300px 0px">
-        <ul className="max-w-7xl mx-auto px-8 lg:px-4 xl:px-0 font-inter -mt-56 grid sm:grid-cols-2 lg:grid-cols-4 gap-5">
-          {FEATURES.map((feature) => (
-            <FeatureItem
-              key={feature.title}
-              title={feature.title}
-              icon={feature.icon}
-              description={feature.description}
-            />
-          ))}
-        </ul>
-      </SlideUp>
+      <ul className="max-w-7xl mx-auto px-4 xl:px-0 font-inter -mt-56 grid grid-cols-2 lg:grid-cols-4 gap-2 md:gap-5">
+        {FEATURES.map((feature) => (
+          <FeatureItem
+            key={feature.title}
+            title={feature.title}
+            icon={feature.icon}
+            description={feature.description}
+          />
+        ))}
+      </ul>
     </section>
   );
 };
@@ -42,10 +39,12 @@ type FeatureItem = {
 
 const FeatureItem = ({ title, icon, description }: FeatureItem) => {
   return (
-    <li className="font-poppins tracking-wide rounded-lg w-full flex flex-col bg-teal-700 px-5 py-10">
+    <li className="font-poppins tracking-wide rounded-lg w-full flex flex-col bg-teal-700 px-2 md:px-5 py-8 md:py-10">
       <div className="mx-auto">{icon}</div>
-      <h2 className="text-xl text-yellow-400 mt-5">{title}</h2>
-      <p className="mt-5 text-neutral-200">{description}</p>
+      <h2 className="text-lg md:text-xl text-yellow-400 mt-5">{title}</h2>
+      <p className="text-sm md:text-base mt-5 text-neutral-200">
+        {description}
+      </p>
     </li>
   );
 };
