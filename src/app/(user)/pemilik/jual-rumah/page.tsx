09798be1a1ea73c93990710263839b26483b6999
Zulfikar Muhamad <zul.m@icloud.com>
2024-01-01 22:56:12 +0800

    Guna sonnar untuk error msg

--- src/app/(user)/pemilik/jual-rumah/page.tsx
+++ src/app/(user)/pemilik/jual-rumah/page.tsx
@@ -2,6 +2,7 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { useState } from "react";
+import { Toaster, toast } from "sonner";
 
 function isInputNamedElement(
   e: Element
@@ -17,10 +18,44 @@ const jualRumahContent = {
 };
 
 const JualRumah = () => {
-  const [state, setState] = useState<string>();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [message, setMessage] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   async function handleOnSubmit(e: React.SyntheticEvent<HTMLFormElement>) {
     e.preventDefault();
+    setIsSubmitting(true);
+
+    const promise = () =>
+      new Promise((resolve) => setTimeout(() => resolve({}), 2000));
+
+    if (name.length < 1) {
+      toast.error("Isi nama anda dengan betul.");
+      setIsSubmitting(false);
+      return;
+    } else if (email.length < 1) {
+      toast.error("Isi email anda dengan betul.");
+      setIsSubmitting(false);
+      return;
+    } else if (phone.length < 1) {
+      toast.error("Isi nombor telefon anda dengan betul.");
+      setIsSubmitting(false);
+      return;
+    } else if (address.length < 1) {
+      toast.error("Isi alamat anda dengan betul.");
+      setIsSubmitting(false);
+      return;
+    } else
+      toast.promise(promise, {
+        loading: "Sila tunggu sebentar...",
+        success:
+          "Terima kasih kerana menghubungi kami! Email pengesahan telah dihantar ke alamat email anda.",
+        error: "Tidak berjaya! Hubungi atau Whatsapp kami di +60176515784",
+        duration: 10000,
+      });
 
     const formData: Record<string, string> = {};
 
@@ -30,8 +65,6 @@ const JualRumah = () => {
         if (!field.name) return;
         formData[field.name] = field.value;
       });
-
-    setState("loading");
 
     await fetch("/api/email", {
       method: "POST",
@@ -44,7 +77,12 @@ const JualRumah = () => {
       }),
     });
 
-    setState("ready");
+    setName("");
+    setEmail("");
+    setPhone("");
+    setAddress("");
+    setMessage("");
+    setIsSubmitting(false);
   }
 
   return (
@@ -117,7 +155,7 @@ const JualRumah = () => {
             </div>
             <div className="flex-1 lg:w-6/12">
               <form
-                className="h-full px-4 bg-white py-14 lg:p-14 font-inter"
+                className="h-full px-4 bg-white py-14 lg:p-14 font-poppins"
                 onSubmit={handleOnSubmit}
               >
                 <h2 className="mb-4 text-3xl text-gray-800">Hubungi Kami</h2>
@@ -133,6 +171,8 @@ const JualRumah = () => {
                     Nama<span className="text-red-600">*</span>
                   </label>
                   <input
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
                     id="firstName"
                     name="firstName"
                     placeholder="Fikry Zulkifly"
@@ -148,6 +188,9 @@ const JualRumah = () => {
                     Email<span className="text-red-600">*</span>
                   </label>
                   <input
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    type="email"
                     id="email"
                     name="email"
                     placeholder="fikryzulkiflyproperty@gmail.com"
@@ -159,12 +202,17 @@ const JualRumah = () => {
                     htmlFor="phoneNo"
                     className="block mb-2 text-base text-gray-600"
                   >
-                    Nombor Telefon<span className="text-red-600">*</span>
+                    Nombor Telefon<span className="text-red-600">* </span>
+                    <span className="text-xs">(Format: 0123456789)</span>
                   </label>
                   <input
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                    type="tel"
+                    pattern="[0-9]{10,11}"
                     id="phoneNo"
                     name="phoneNo"
-                    placeholder="+60176515784"
+                    placeholder="0123456789"
                     className="rounded-none appearance-none !text-base p-2 outline-1 mt-1 px-3 py-2 bg-white border shadow border-slate-300 placeholder-slate-400 focus:outline-none focus:border-teal-600 focus:ring-teal-600 block w-full sm:text-sm focus:ring-1 outline-gray-300"
                   ></input>
                 </div>
@@ -176,6 +224,8 @@ const JualRumah = () => {
                     Alamat Hartanah<span className="text-red-600">*</span>
                   </label>
                   <input
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
                     id="address"
                     name="address"
                     placeholder="No. 8, Bandar Sri Damansara, Kuala ..."
@@ -190,6 +240,8 @@ const JualRumah = () => {
                     Mesej / Pesanan
                   </label>
                   <textarea
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
                     id="message"
                     name="message"
                     cols={30}
@@ -199,9 +251,10 @@ const JualRumah = () => {
                   ></textarea>
                 </div>
                 <div className="block px-0 mb-6">
+                  <Toaster richColors />
                   <button
-                    className="cursor-pointer w-full block transition-all duration-300 ease-in-out text-[11.5px] tracking-[2px] font-bold uppercase bg-teal-600 shadow py-4 px-5 text-white hover:bg-white hover:text-teal-600 hover:shadow-2xl"
-                    disabled={state === "loading"}
+                    className="cursor-pointer w-full block transition-all duration-300 ease-in-out text-[11.5px] tracking-[2px] font-bold uppercase bg-teal-600 shadow py-4 px-5 text-white hover:bg-white hover:text-teal-600 hover:shadow-2xl disabled:bg-gray-500"
+                    disabled={isSubmitting}
                   >
                     Hantar
                   </button>
